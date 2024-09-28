c47a6e100a3411022c662ed5ad8b5443fe998abc
Ali-Dmerdash <alidmerdash@gmail.com>
2024-09-28 18:16:06 +0300

    Applied Text-Color and Font-Family Globally

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,20 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material";
+
+export const theme = createTheme({
+  typography: {
+    fontFamily: ["Outfit", "sans-serif"].join(","),
+  },
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </StrictMode>
 );
