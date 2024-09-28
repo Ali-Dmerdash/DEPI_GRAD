afcca862765a6b38c2153c9813c4c3c28a57ba02
Ali-Dmerdash <alidmerdash@gmail.com>
2024-09-28 20:31:59 +0300

    chore: Finish UI for Login Page(#11)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "react-router-dom";
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Login />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
