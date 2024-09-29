2bbc3acb4e7d8fefabd9bbce417cd742a920bab6
Ali-Dmerdash <alidmerdash@gmail.com>
2024-09-29 17:14:13 +0300

    chore: Finish UI for Profile Page(#8)

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import Profile from "./pages/profile";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Profile />
+    </>
+  );
 }
 
 export default App;
