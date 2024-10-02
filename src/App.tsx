ce977879daee7308b536a8daedc6a8366ef26241
Alialdin66 <145298096+Alialdin66@users.noreply.github.com>
2024-10-02 16:00:13 +0300

    chore: finish the UI OF DashboardPage (#14)

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
+import Dashboard from "./Component/AdminDashboard/Dashboard";
 
 function App() {
-  return <></>;
+  return <>
+      <Dashboard/>
+  </>;
 }
 
 export default App;
