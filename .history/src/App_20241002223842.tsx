0bfb874a040714342a6e5c41cf6dbef40ab5a25e
salahelden1212 <salahelkalyouby2@gmail.com>
2024-10-02 22:46:06 +0300

    chore:start aboutus page #6

--- .history/src/App_20241002223842.tsx
+++ .history/src/App_20241002223842.tsx
@@ -4,7 +4,7 @@ import { library } from "@fortawesome/fontawesome-svg-core";
 import { fab } from "@fortawesome/free-brands-svg-icons";
 import { fas } from "@fortawesome/free-solid-svg-icons";
 import { far } from "@fortawesome/free-regular-svg-icons";
-import AboutUs from "./pages/aboutus/index.tsx";
+import AboutUs from "../src/pages/index.tsx";
 function App() {
   return (
     <>
