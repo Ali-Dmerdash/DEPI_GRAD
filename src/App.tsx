ab482067a64047f02bd2a496a547492a21a8f372
MohamedQushta <m.ashraf2121@nu.edu.eg>
2024-10-05 22:21:29 +0300

    finished appointments table

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,15 @@ import { far } from "@fortawesome/free-regular-svg-icons";
 import AddDoctors from "./pages/addDoctors/AddDoctors";
 import ListDoctors from "./pages/listDoctors/ListDoctors";
 import { Box, Container } from "@mui/material";
+import AppointmentsTable from "./pages/appointmentsTable/AppointmentsTable";
 
 function App() {
   return (
     <>
       {/* <Navbar />
       <Sidebar /> */}
-      <AddDoctors />
-
+      {/* <AddDoctors /> */}
+      <AppointmentsTable />
       {/* <ListDoctors /> */}
     </>
   );
